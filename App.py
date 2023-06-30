eeb3a312e7674c091f2f4f99bb08e042b0053971
Vares-maker <123036053+Vares-maker@users.noreply.github.com>
2023-06-30 22:08:04 +0530

    Update App.py

--- App.py
+++ App.py
@@ -1,9 +1,11 @@
 import streamlit as st
+import keytotext
 from keytotext import pipeline
 Keywords = []
 Splited = []
 nlp = pipeline("mrm8488/t5-base-finetuned-common_gen")
-st.markdown("<h1 style='text-align: center; color: White;'>Sentence Maker</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center; color: Black;'>Varesh AI</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center; color: Black;'>Sentence Maker</h1>", unsafe_allow_html=True)
 st.info("To type the keywords type with comma and space after each comma Example:- Key, Word")
 col1, col2, col3 = st.columns(3)
 
